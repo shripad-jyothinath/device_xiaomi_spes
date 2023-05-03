f2e20e3f92203328a1183a956c8aa898e4c3fc0b
shripad <71135716+shripad-jyothinath@users.noreply.github.com>
2023-05-03 16:43:54 +0530

    Added libdisplayconfig.qti

--- device.mk
+++ device.mk
@@ -176,6 +176,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    libdisplayconfig.qti \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     memtrack.bengal
