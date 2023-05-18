dc2b9599f5fec44f2ca53fa0befa61fa2556b4d2
shripad-jyothinath <shripadsuper@gmail.com>
2023-05-18 13:18:50 +0000

    RisingOS bringup

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -25,4 +25,44 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 12 SKQ1.211103.001 V13.0.4.0.SGCMIXM release-keys"
 
+# Rising Stuff
+RISING_OFFICIAL := false
+
+# Aperture Camera
+TARGET_BUILD_APERTURE_CAMERA := true
+
+# Blur Support
+TARGET_ENABLE_BLUR := true
+
+# Gapps
+WITH_GMS := true
+TARGET_CORE_GMS := true
+
+# extra flag under TARGET_CORE_GMS
+TARGET_CORE_GMS_EXTRAS := true
+
+#UDFPS animations
+TARGET_HAS_UDFPS := false
+
+# Spoof build description/fingerprint as pixel device
+TARGET_USE_PIXEL_FINGERPRINT := true
+
+#Assistant flags
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+#Face unlock supported
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+#Dolby flags
+TARGET_EXCLUDES_AUDIOFX := true
+
+#Chipset
+RISING_CHIPSET := "snapdragon 680"
+
+#Maintainer
+RISING_MAINTAINER := "Shripad J"
+
+#Package type
+RISING_PACKAGE_TYPE := PIXEL
+
 BUILD_FINGERPRINT := Redmi/spes_global/spes:12/SKQ1.211103.001/V13.0.4.0.SGCMIXM:user/release-keys
