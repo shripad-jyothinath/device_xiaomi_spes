ef4f6182d4cafa30cab3e4badce408774ce1ae86
shripad <shripadsuper@gmail.com>
2023-07-08 21:12:06 +0530

    vendorsetup.sh: Update for ota

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,22 +20,26 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 
-echo -e "${blue}Syncing bootctrl(1/5)${blue}"
+echo -e "${blue}Syncing bootctrl(1/6)${blue}"
 #Sync qcom-caf bootctrl
 rm -rf hardware/qcom-caf/bootctrl
 git clone https://github.com/LineageOS/android_hardware_qcom_bootctrl.git -b lineage-19.1-caf hardware/qcom-caf/bootctrl
-echo -e "${blue}Syncing hardware(2/5)${blue}"
+echo -e "${blue}Syncing hardware(2/6)${blue}"
 #sync Hardware tree
 git clone https://github.com/CHRISL7/hardware_xiaomi.git hardware/xiaomi 
-echo -e "${blue}Syncing vendor(3/5)${blue}"
+echo -e "${blue}Syncing vendor(3/6)${blue}"
 #Sync Vendor tree
 git clone https://github.com/shripad-jyothinath/vendor_xiaomi_spes.git vendor/xiaomi/spes
-echo -e "${blue}Syncing Kernel(4/5)${blue}"
+echo -e "${blue}Syncing Kernel(4/6)${blue}"
 #Sync Kernel tree
 git clone https://github.com/Jabiyeff/kernel_xiaomi_spes_header.git kernel/xiaomi/spes
 
-echo -e "${blue}Syncing Camera(5/5)${blue}"
+echo -e "${blue}Syncing Camera(5/6)${blue}"
 rm -rf device/xiaomi/sm6225-common-miuicamera
 git clone https://github.com/CHRISL7/android_device_xiaomi_sm6225-common-miuicamera.git device/xiaomi/sm6225-common-miuicamera
+
+echo -e "${blue}Updating ota(6/6)${blue}"
+rm -rf vendor/risingOTA
+git clone https://github.com/shripad-jyothinath/android_vendor_RisingOTA vendor/risingOTA
 
 echo -e "${color}Finished sync!${end}"
