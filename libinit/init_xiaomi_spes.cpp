57b002b170af13973501acdb70006ff5b9f90ab5
shripad-jyothinath <shripadsuper@gmail.com>
2023-07-13 17:16:17 +0530

    Revert "spes: libinit: Workaround safetynet"
    Fixes error:
    
        device/xiaomi/spes/libinit/init_xiaomi_spes.cpp:59:5: error: use of undeclared identifier 'property_override'
        property_override("ro.boot.verifiedbootstate", "green");
    
    This reverts commit 017199effd2f71f41b648c29b4689b161f16bf72.

--- libinit/init_xiaomi_spes.cpp
+++ libinit/init_xiaomi_spes.cpp
@@ -54,7 +54,4 @@ static const std::vector<variant_info_t> variants = {
 void vendor_load_properties() {
     search_variant(variants);
     set_dalvik_heap();
-    
-    // SafetyNet workaround
-    property_override("ro.boot.verifiedbootstate", "green");    
 }
