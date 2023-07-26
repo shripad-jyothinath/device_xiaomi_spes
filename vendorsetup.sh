8e3329d408803d2b18c6998b26db877d1ca75548
shripad <shripadsuper@gmail.com>
2023-07-26 16:53:31 +0530

    Spes: Vendorsetup.sh: Fix chromium webview error
    Rising OS has a chromium webview error, to fix it we need to clone from lineage
    
    Signed-off-by: shripad-jyothinath <shripadsuper@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,26 +20,34 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 
-echo -e "${blue}Syncing bootctrl(1/6)${blue}"
+
+echo -e "${blue}Syncing bootctrl(1/7)${blue}"
 #Sync qcom-caf bootctrl
 rm -rf hardware/qcom-caf/bootctrl
 git clone https://github.com/LineageOS/android_hardware_qcom_bootctrl.git -b lineage-19.1-caf hardware/qcom-caf/bootctrl
-echo -e "${blue}Syncing hardware(2/6)${blue}"
+echo -e "${blue}Syncing hardware(2/7)${blue}"
 #sync Hardware tree
 git clone https://github.com/CHRISL7/hardware_xiaomi.git hardware/xiaomi 
-echo -e "${blue}Syncing vendor(3/6)${blue}"
+echo -e "${blue}Syncing vendor(3/7)${blue}"
 #Sync Vendor tree
 git clone https://github.com/shripad-jyothinath/vendor_xiaomi_spes.git vendor/xiaomi/spes
-echo -e "${blue}Syncing Kernel(4/6)${blue}"
+echo -e "${blue}Syncing Kernel(4/7)${blue}"
 #Sync Kernel tree
 git clone https://github.com/Jabiyeff/kernel_xiaomi_spes_header.git kernel/xiaomi/spes
 
-echo -e "${blue}Syncing Camera(5/6)${blue}"
+echo -e "${blue}Syncing Camera(5/7)${blue}"
 rm -rf device/xiaomi/sm6225-common-miuicamera
 git clone https://github.com/CHRISL7/android_device_xiaomi_sm6225-common-miuicamera.git device/xiaomi/sm6225-common-miuicamera
 
-echo -e "${blue}Updating ota(6/6)${blue}"
+echo -e "${blue}Updating ota(6/7)${blue}"
 rm -rf vendor/risingOTA
 git clone https://github.com/shripad-jyothinath/android_vendor_RisingOTA vendor/risingOTA
+
+echo "${blue}Syncing webview (7/7)${blue}"
+rm -rf external/chromium-webview/prebuilt/*
+rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+
+git clone https://github.com/LineageOS/android_external_chromium-webview_prebuilt_arm64.git external/chromium-webview/prebuilt/arm64
 
 echo -e "${color}Finished sync!${end}"
